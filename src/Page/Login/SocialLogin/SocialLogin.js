dc2c76b7d94f23d6709a48d1cb7347277735f622
Abdul Awal Suhail <abdulawalsuhail>
2022-04-17 23:26:43 +0600

    helmet pakage i and logout page added

--- src/Page/Login/SocialLogin/SocialLogin.js
+++ src/Page/Login/SocialLogin/SocialLogin.js
@@ -35,17 +35,17 @@ const SocialLogin = () => {
                 <button
                     onClick={() => signInWithGoogle()}
                     className='btn btn-info w-50 d-block mx-auto my-2'>
-                    <img style={{ width: '30px' }} src={google} alt="" />
+                    <img style={{ width: '30px' }} src='' alt="" />
                     <span className='px-2'>Google Sign In</span>
                 </button>
                 <button className='btn btn-info w-50 d-block mx-auto my-2'>
-                    <img style={{ width: '30px' }} src={facebook} alt="" />
+                    <img style={{ width: '30px' }} src='' alt="" />
                     <span className='px-2'>Facebook Sign In</span>
                 </button>
                 <button
                     onClick={() => signInWithGithub()}
                     className='btn btn-info w-50 d-block mx-auto'>
-                    <img style={{ width: '30px' }} src={github} alt="" />
+                    <img style={{ width: '30px' }} src='' alt="" />
                     <span className='px-2'>Github Sign In</span>
                 </button>
             </div>
